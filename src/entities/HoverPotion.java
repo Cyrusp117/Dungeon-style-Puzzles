dc99f541d8ebfa67cc5448269990cd2f24f23465
AjayUmasankar <z5025214@student.unsw.edu.au>
2018-09-13 19:14:47 +1000

    Basic hoverpotion implementation
    Pit interaction with playre should check if player has a hover potion

---
+++ src/entities/HoverPotion.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class HoverPotion extends Entity {
+	
+	public HoverPotion(Coordinate position) {
+		super(position);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		return "HoverPotion";
+	}
+	
+}
