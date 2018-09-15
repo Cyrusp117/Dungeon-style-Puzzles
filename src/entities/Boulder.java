915b653846a67a8524461ec0e596fa4b73046ba0
liuchenyu23 <30437047+liuchenyu23@users.noreply.github.com>
2018-09-16 00:33:09 +1000

    merge conflicts fixed

--- src/entities/Boulder.java
+++ src/entities/Boulder.java
@@ -32,3 +32,33 @@ public class Boulder extends Entity {
 	}
 	
 }
+=======
+
+package entities;
+
+import java.awt.event.KeyEvent;
+
+public class Boulder extends Entity {
+	public Boulder(Coordinate position) {
+		super(position);
+		this.keyCode = KeyEvent.VK_B;
+	}
+	
+	public boolean interactWithPlayer(int x,int y) {
+		int newX = position.getxPosition() + x;
+	    int newY = position.getyPosition() + y;
+	    Coordinate newPos = new Coordinate(newX, newY);
+	    this.setPosition(newPos);
+	    return false;
+	}
+	
+	public boolean interactWithBomb() {
+		//delete this boulder
+		return true;
+	}
+	
+	public String getName() {
+		return "Boulder";
+	}
+	
+}
--- src/entities/Pit.java
+++ src/entities/Pit.java
@@ -1,4 +1,5 @@
 
+
 package entities;
 
 import java.awt.event.KeyEvent;
@@ -30,3 +31,38 @@ public class Pit extends Entity{
 	}
 	
 }
+=======
+
+package entities;
+
+import java.awt.event.KeyEvent;
+
+public class Pit extends Entity{
+	public Pit(Coordinate position) {
+		super(position);
+		this.keyCode = KeyEvent.VK_P;
+	}
+
+	public boolean interactWithPlayer(Player player,int x, int y) {
+		if(player.hasItem("HoverPotion")) {
+			int newX = position.getxPosition() + x;
+		    int newY = position.getyPosition() + y;
+		    Coordinate newPos = new Coordinate(newX, newY);
+		    player.setPosition(newPos);
+		}else{
+			player.setState(0); // player is dead
+		}
+		return false;
+	}
+	
+	public boolean interactWithBoulder(Boulder boulder) {
+		//delete boulder
+		return true;
+	}
+	
+	public String getName() {
+		return "Pit";
+	}
+	
+}
+
