8a56adc5d0cb34d8963bcca94b8e7cda72115441
david <david@Kindunos-Lap>
2018-10-21 16:32:26 +1100

    Enemy cleaned up a bit

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -18,22 +18,14 @@ public abstract class Enemy extends Entity{
 		g.addCoordinate(position); //doing this before hand solves not being able to use in targetSpace stuff
 		g.generateEdges();
 		Coordinate move = getTargetSpace(co,g,closestPickup, entities);
-		//g.addCoordinate(position); //need this?
-		//g.generateEdges();
-		//boolean check = false;
+		
 		if (move == co && g.isAdjacent(position, co)) {
 			move = position;
 		} else if (move != position){
-			//test 
-			System.out.println("Move coordinate is : (" +  move.getX() +"," + move.getY() + ")");
-			System.out.println("position coordinate is : (" +  position.getX() +"," + position.getY() + ")");
+			
 		    move = g.BFS(this.position, move,co); 
-		    //check = true;
+		  
 		} 
-		//if attempt is made to move to spot near player but cannot, will attempt any move it can do player
-		//if (check && move == position) {
-		//	move = g.BFS(this.position, co, co);
-		//}
 		
 		
 		return move;
